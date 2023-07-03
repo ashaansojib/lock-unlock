b4578453db2838a6fffc0dbc9096de5ce0eda9e4
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-07-03 16:20:11 +0600

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../pages/home/Home';
+import NavBar from '../pages/navigation/NavBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar />
+            <Home />
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/navigation/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const NavBar = () => {
+    const navItem = <>
+        <li><a>Item 1</a></li>
+        <li><a>Item 3</a></li>
+    </>
+    return (
+        <nav className='bg-base-200'>
+            <div className="navbar my-container">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navItem}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">Lock Unlocker</a>
+                </div>
+                <div className="navbar-end">
+                <div className="hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navItem}
+                    </ul>
+                </div>
+                    <a className="btn">Signup</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
